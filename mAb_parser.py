e0c24a536a80b3cac599d5b93d6c98b584c4ba07
Sepid Mazrouee <140639319+smazroueeods@users.noreply.github.com>
2024-11-25 11:17:22 -0800

    Update mAb_parser.py

--- mAb_parser.py
+++ mAb_parser.py
@@ -85,6 +85,109 @@ def load_data(data_folder: str):
                 'predicate': 'causes'
             }
 
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "ChemicalMiture",
+#                     'id': row['mab_id'],
+#                     "name": row['mab_name'],
+#                 },
+#                 'object': {
+#                     "type": "Virus",
+#                     'id': int(row['virus_id']),
+#                     'name': row['virus_name'],
+#                 },
+#                 "pubmed": <pubmed_id>,
+#                 'predicate': 'targets'
+#             }
+
+
+
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "virus",
+#                     "name": row['virus_name'],
+#                     'id': row['virus_id'],
+#                 },
+#                 'object': {
+#                     "type": "disease",
+#                     "id": int(row['disease_id']),
+#                     'name': row['disease_name'],
+#                 },
+#                 'predicate': 'causes'
+#             }
+
+
+
+
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "disease",
+#                     "id": row['disease_id'],
+#                     "name": row['disease_name'],
+#                 },
+#                 'object': {
+#                     "type": "phenotypic_feature",
+#                     'id': int(row['phetotypic_feature_id']),
+#                     'name': row['phenotypic_feature_name'],
+#                 },
+#                 'predicate': 'exhibits'
+#             }
+
+
+
+
+
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "Antibody",
+#                     "id": row['mab_id'],
+#                     "name": row['mab_name'],
+#                 },
+#                 'object': {
+#                     "type": "phenotypic_feature",
+#                     "id": int(row['symptom_id']),
+#                     "name": row['symptom_name'],
+#                 },
+#                 'predicate': 'ameliorates'
+#             }
+
+
+
+
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "ChemicalMixture",
+#                     'id': row['mab_id'],
+#                     "name": row['mab_name'],
+#                 },
+#                 'object': {
+#                     "type": "disease",
+#                     'id': int(row['disease_id']),
+#                     'name': row['disease_name'],
+#                 },
+#                 'predicate': 'treats'
+#             }
+
+
+
+
+# '_id': doc_id,
+#                 'subject': {
+#                     "type": "ChemicalMixture",
+#                     'id': row['mab_id'],
+#                     "name": row['mab_name'],
+#                 },
+#                 'object': {
+#                     "type": "gene",
+#                     'id': int(row['gene_id']),
+#                     'name': row['gene_name'],
+#                 },
+#                 'predicate': 'interacts with'
+#             }
+
+
+
+    
     for doc_id in docs:
         yield docs[doc_id]
 
